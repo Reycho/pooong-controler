b7e5c6a599276f041bee96cf40897911d660c816
Reycho <62747204+Reycho@users.noreply.github.com>
2022-06-28 14:19:34 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,40 @@
 input.onGesture(Gesture.TiltLeft, function () {
-    radio.sendString("left")
+    radio.sendValue("bleft", 0)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        `)
+    basic.pause(350)
+    basic.clearScreen()
 })
 input.onGesture(Gesture.TiltRight, function () {
-    radio.sendString("right")
+    radio.sendValue("bright", 0)
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        `)
+    basic.pause(350)
+    basic.clearScreen()
 })
+let vol = 0
+music.setVolume(vol)
+vol = 0
 radio.setGroup(69)
-basic.showLeds(`
-    # # # # #
-    # . . . .
-    # . . . .
-    # . . . .
-    # # # # #
-    `)
 basic.forever(function () {
-    led.toggle(4, 2)
+    led.toggle(2, 2)
     basic.pause(100)
+    if (input.buttonIsPressed(Button.A)) {
+        vol += 10
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        vol += -10
+    }
 })
